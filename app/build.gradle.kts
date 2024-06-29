bf46eb07669e512858cd303bcb0b2017c1aad53c
khondokermoin <mouin2k19@gmail.com>
2024-06-29 12:47:28 +0600

    complite loginActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
